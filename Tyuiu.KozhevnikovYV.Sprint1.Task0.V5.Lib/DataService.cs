ecc9ae69ca6dc6c140d1a6792a46c2488a05f0dc
Ярослав <kozheva19@mail.ru>
2025-09-24 12:20:17 +0500

    Add project files.

---
+++ Tyuiu.KozhevnikovYV.Sprint1.Task0.V5.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint1.Task0.V5.Lib
+{
+    using tyuiu.cources.programming.interfaces.Sprint1;
+    public class DataService : ISprint1Task0V5
+    {
+        public double Calculate()
+        {
+            return (1 + 2) * (1 + 9 / 3);
+        }
+    }
+}
---
+++ Tyuiu.KozhevnikovYV.Sprint1.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint1.Task0.V5.Test
+{
+    using Tyuiu.KozhevnikovYV.Sprint1.Task0.V5.Lib;
+
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(12, res);
+     
+        
+        }
+    }
+}
