216bd2ae3b4c443274e394ef3c6fbb68ca68daff
Ярослав <kozheva19@mail.ru>
2025-09-29 01:43:48 +0500

    Task6

---
+++ Tyuiu.KozhevnikovYV.Sprint1.Task6.V16.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint1.Task6.V16.Lib
+{
+    using System.ComponentModel.DataAnnotations;
+    using tyuiu.cources.programming.interfaces.Sprint1;
+    using static System.Net.Mime.MediaTypeNames;
+
+    public class DataService : ISprint1Task6V16
+    {
+        public bool CheckSpecSymbols(string value)
+        {
+            if (value.Contains("!"))
+            {
+                return true;
+            }
+            if (value.Contains("?"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Tyuiu.KozhevnikovYV.Sprint1.Task6.V16.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.KozhevnikovYV.Sprint1.Task6.V16.Test
+{
+    using Tyuiu.KozhevnikovYV.Sprint1.Task6.V16.Lib;
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSymbols()
+        {
+            DataService ds = new DataService();
+            string strtest = "123456?";
+            bool res = ds.CheckSpecSymbols(strtest);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
